ef00a65fcc77ed7f7b30ddc9e24247eca27e6011
Zoha Fard <72476695+zohaf@users.noreply.github.com>
2021-02-19 15:51:49 +1100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
+
+// import usePlacesAutocomplete, {
+//   getGeocode,
+//   getLatLng,
+// } from "use-places-autocomplete";
+// import useOnclickOutside from "react-cool-onclickoutside";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>map of the hood</p>
     </div>
   );
 }
